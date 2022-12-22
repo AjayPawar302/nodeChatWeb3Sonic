edaa33e193ad91e2a3c87b8697d6a4408f9713d2
unknown <ajay.pawar.cse19@gmail.com>
2022-12-22 10:37:53 +0530

    22/12/22

--- controller/user_room.controler.js
+++ controller/user_room.controler.js
@@ -10,26 +10,49 @@ exports.userAvailableRoom = async (req, res) => {
         type: QueryTypes.SELECT,
     });
     if (user.length > 0) {
+
+        const [results, metadata] = await db.sequelize.query(
+            "SELECT * FROM users JOIN customer_profiles ON customer_profiles.user_id = users.id where users.id= :user_id"
+            ,
+            {
+                replacements: { user_id: [req.body.receiver_id] },
+                type: QueryTypes.SELECT,
+            });
+        console.log(JSON.stringify(results.profile_image, null, 2));
+        let userInfo = await db.user.findOne({ where: { id: req.body.receiver_id } })
         res.status(200).send({
             room_id: user[0].id,
-    })
+            ...userInfo.dataValues,
+            image_path: results.profile_image
+        })
     }
     else {
 
         isNewRecord = await createRoom(req.body.sender_id, req.body.receiver_id);
-       
+
         if (isNewRecord) {
             let newAddedRoomMessageList = await db.sequelize.query('select * from user_rooms where sender_id IN(:sender_id) and receiver_id IN(:receiver_id)', {
                 replacements: { sender_id: [req.body.sender_id, req.body.receiver_id], receiver_id: [req.body.sender_id, req.body.receiver_id] },
                 type: QueryTypes.SELECT,
             });
+
+            const [results, metadata] = await db.sequelize.query(
+                "SELECT * FROM users JOIN customer_profiles ON customer_profiles.user_id = users.id where users.id= :user_id"
+                ,
+                {
+                    replacements: { user_id: [req.body.receiver_id] },
+                    type: QueryTypes.SELECT,
+                });
+            let userInfo = await db.user.findOne({ where: { id: req.body.receiver_id } })
             res.status(200).send({
-                        room_id: newAddedRoomMessageList[0].id,
-                })
-          
-        }else{
+                room_id: newAddedRoomMessageList[0].id,
+                ...userInfo.dataValues,
+                image_path: results.profile_image
+            })
+
+        } else {
             res.status(400).send({
-                message:'filed to create room'
+                message: 'filed to create room'
             })
         }
     }
